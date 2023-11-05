999ff31ed0f31407eed3ad16cce922baedd7000a
CxMax222 <53593787+CxMax222@users.noreply.github.com>
2023-11-05 12:43:22 +0800

    Update DINA.py
    adding annotation

--- CasualCDF/EduCDM/DINA/GD/DINA.py
+++ CasualCDF/EduCDM/DINA/GD/DINA.py
@@ -111,6 +111,8 @@ class DINA(CDM):
             losses = []
             for batch_data in tqdm(train_data, "Epoch %s" % e):
                 user_id, item_id, knowledge, response, diff = batch_data
+                mean_diff = torch.mean(diff)
+                diff = mean_diff.expand(diff.size())
                 user_id: torch.Tensor = user_id.to(device)
                 item_id: torch.Tensor = item_id.to(device)
                 knowledge: torch.Tensor = knowledge.to(device)
